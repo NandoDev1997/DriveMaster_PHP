66e8e54bd7474a605c09dde03082c82ac5ecf8fd
Fernando Morales <luisfernandomoralescortes1997@gmail.com>
2021-03-29 22:56:23 -0600

    index test

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php 
 require_once ('config/config.php');
 require_once ('control/plantilla_control.php');
+require_once ('lib/database.php');
 $plantilla = new ControladorPlantilla();
-$plantilla->obtenerPlantilla();
+//$plantilla->obtenerPlantilla();
+$db = new Database();
+$query = 'SELECT * FROM usuarios';
+$users = $db->getRow($query);
+echo '<pre>';
+print_r($users);
+echo '</pre>';
+
 ?>
